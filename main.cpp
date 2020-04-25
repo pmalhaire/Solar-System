f15bf6d14b22125566ab9ccf313f7eee5819249b
pmalhaire <pierre.malhaire@gmail.com>
2020-04-25 11:39:57 +0200

    lint and add build script

--- main.cpp
+++ main.cpp
@@ -4,97 +4,94 @@
 #include <cstdlib>
 #include "tga.h"
 
-static int day = 0,month=0,year = 0;
+static int day = 0, month = 0, year = 0;
 
 struct Star
 {
     float x, y;
     unsigned char r, g, b, a;
 };
-std::vector< Star > stars;
+std::vector<Star> stars;
 
-TGA* sun;
-TGA* earth;
-TGA* moon;
+TGA *sun;
+TGA *earth;
+TGA *moon;
 
 void init(void)
 {
-    glClearColor(0.0,0.0,0.0,0.0);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
     glShadeModel(GL_FLAT);
     sun = new TGA("images/sun.tga");
     moon = new TGA("images/moon.tga");
     earth = new TGA("images/earth.tga");
-    //glOrtho(-1.0,1.0,-1.0,1.0,-1.0,1.0);
+    // glOrtho(-1.0,1.0,-1.0,1.0,-1.0,1.0);
 
-    //Lighting set up
-    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER,GL_TRUE);
+    // Lighting set up
+    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
 
-    //Set lighting intensity and color
-    GLfloat qaAmbientLight[] = {0.2,0.2,0.2,1.0};
-    GLfloat qaDiffuseLight[] = {0.8,0.8,0.8,1.0};
-    GLfloat qaSpecularLight[] = {1.0,1.0,1.0,1.0};
-    glLightfv(GL_LIGHT0,GL_AMBIENT,qaAmbientLight);
-    glLightfv(GL_LIGHT0,GL_DIFFUSE,qaDiffuseLight);
-    glLightfv(GL_LIGHT0,GL_SPECULAR,qaSpecularLight);
+    // Set lighting intensity and color
+    GLfloat qaAmbientLight[] = {0.2, 0.2, 0.2, 1.0};
+    GLfloat qaDiffuseLight[] = {0.8, 0.8, 0.8, 1.0};
+    GLfloat qaSpecularLight[] = {1.0, 1.0, 1.0, 1.0};
+    glLightfv(GL_LIGHT0, GL_AMBIENT, qaAmbientLight);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, qaDiffuseLight);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, qaSpecularLight);
 
-    //Set the light position
-    GLfloat qaLightPosition[] = {0.0,0.0,0.0,1.0};
-    glLightfv(GL_LIGHT0,GL_POSITION,qaLightPosition);
-
+    // Set the light position
+    GLfloat qaLightPosition[] = {0.0, 0.0, 0.0, 1.0};
+    glLightfv(GL_LIGHT0, GL_POSITION, qaLightPosition);
 }
 
 void display(void)
 {
 
-    //Set material properties
-    GLfloat qaWhite[] = {1.0,1.0,1.0,1.0};
+    // Set material properties
+    GLfloat qaWhite[] = {1.0, 1.0, 1.0, 1.0};
 
-    glMaterialfv(GL_FRONT,GL_AMBIENT,qaWhite);
-    glMaterialfv(GL_FRONT,GL_DIFFUSE,qaWhite);
-    glMaterialfv(GL_FRONT,GL_SPECULAR,qaWhite);
-    glMaterialf(GL_FRONT,GL_SHININESS,60.0);
+    glMaterialfv(GL_FRONT, GL_AMBIENT, qaWhite);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, qaWhite);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, qaWhite);
+    glMaterialf(GL_FRONT, GL_SHININESS, 60.0);
 
-   // glNormal3f(1.0,0.0,1.0);
+    // glNormal3f(1.0,0.0,1.0);
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
 
-    //Draw Stars
-    glColor3ub( 255, 255, 255 );
-    glEnableClientState( GL_VERTEX_ARRAY );
-    glEnableClientState( GL_COLOR_ARRAY );
-    glVertexPointer( 2, GL_FLOAT, sizeof(Star), &stars[0].x );
-    glColorPointer( 4, GL_UNSIGNED_BYTE, sizeof(Star), &stars[0].r );
-    glPointSize( 3.0 );
-    glDrawArrays( GL_POINTS, 0, stars.size() );
-    glDisableClientState( GL_VERTEX_ARRAY );
-    glDisableClientState( GL_COLOR_ARRAY );
+    // Draw Stars
+    glColor3ub(255, 255, 255);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    glVertexPointer(2, GL_FLOAT, sizeof(Star), &stars[0].x);
+    glColorPointer(4, GL_UNSIGNED_BYTE, sizeof(Star), &stars[0].r);
+    glPointSize(3.0);
+    glDrawArrays(GL_POINTS, 0, stars.size());
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
 
-
-    //Draw Sun
-    glRotatef((GLfloat) year,0.0,1.0,0.0);
+    // Draw Sun
+    glRotatef((GLfloat)year, 0.0, 1.0, 0.0);
     glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, sun->getTextureHandle());
-    GLUquadricObj* quadric = gluNewQuadric();
-	gluQuadricTexture(quadric, true);
-	gluQuadricNormals(quadric, GLU_SMOOTH);
+    GLUquadricObj *quadric = gluNewQuadric();
+    gluQuadricTexture(quadric, true);
+    gluQuadricNormals(quadric, GLU_SMOOTH);
     gluSphere(quadric, 4.0, 30, 30);
 
-
-    //Draw Earth
-    glRotatef((GLfloat) year,0.0,1.0,0.0);
-    glTranslatef(10.0,0.0,0.0);
-    glRotatef((GLfloat) day,0.0,1.0,0.0);
+    // Draw Earth
+    glRotatef((GLfloat)year, 0.0, 1.0, 0.0);
+    glTranslatef(10.0, 0.0, 0.0);
+    glRotatef((GLfloat)day, 0.0, 1.0, 0.0);
     glBindTexture(GL_TEXTURE_2D, earth->getTextureHandle());
-    gluSphere(quadric,0.7, 30, 30);//EARTH
+    gluSphere(quadric, 0.7, 30, 30);
 
-    //Draw Moon
-    glTranslatef(1.0,0.0,0.0);
-    glRotatef((GLfloat)month,0.0,1.0,0.0);
+    // Draw Moon
+    glTranslatef(1.0, 0.0, 0.0);
+    glRotatef((GLfloat)month, 0.0, 1.0, 0.0);
     glBindTexture(GL_TEXTURE_2D, moon->getTextureHandle());
-    gluSphere(quadric,0.2, 30, 30);//Moon
+    gluSphere(quadric, 0.2, 30, 30);
 
     glPopMatrix();
     glPushMatrix();
@@ -102,36 +99,36 @@ void display(void)
     glutSwapBuffers();
 }
 
-void reshape(int w,int h)
+void reshape(int w, int h)
 {
-    if(h==0)h=1;
+    if (h == 0)
+        h = 1;
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glViewport(0,0,w,h);
-    gluPerspective(50.0,w/(GLfloat)h,3.0,90.0);
+    glViewport(0, 0, w, h);
+    gluPerspective(50.0, w / (GLfloat)h, 3.0, 90.0);
     glMatrixMode(GL_MODELVIEW);
-    gluLookAt(2.0,4.0,20.0,0.0,0.0,0.0,0.0,1.0,0.0);
+    gluLookAt(2.0, 4.0, 20.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
 void idleFunc()
 {
-    year = (year+1) % 360;
-    day = (day+10) % 360;
-    month = (month+3) % 360;
+    year = (year + 1) % 360;
+    day = (day + 10) % 360;
+    month = (month + 3) % 360;
     glutPostRedisplay();
 }
 
-int main(int argc,char** argv)
+int main(int argc, char **argv)
 {
-    glutInit(&argc,argv);
+    glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(1200,800);
-    glutInitWindowPosition(300,150);
+    glutInitWindowSize(1200, 800);
+    glutInitWindowPosition(300, 150);
     glutCreateWindow("Solar system");
 
-
-    //Stars
-    for( size_t i = 0; i < 1000; ++i )
+    // Stars
+    for (size_t i = 0; i < 1000; ++i)
     {
         Star star;
         star.x = -50 + (rand() % 100);
--- tga.cpp
+++ tga.cpp
@@ -10,29 +10,30 @@
 // define the header struct for
 // loading the TGA header info.
 #pragma pack(1) // pack it so that I can have shorts & chars in sequence and read directly from file memory
-struct TGAHeader{
-    char    id_length;
-    char    map_type;
-    char    type;
-    short   map_start;
-    short   map_length;
-    char    map_depth;
-    short   x_origin;
-    short   y_origin;
-    short   width;
-    short   height;
-    char    bpp;
-    char    descriptor_bits;
+struct TGAHeader
+{
+    char id_length;
+    char map_type;
+    char type;
+    short map_start;
+    short map_length;
+    char map_depth;
+    short x_origin;
+    short y_origin;
+    short width;
+    short height;
+    char bpp;
+    char descriptor_bits;
 };
 #pragma pack()
 
-TGA::TGA(const char* imagePath)
+TGA::TGA(const char *imagePath)
 {
-    FILE* file = NULL; // the file handle
-    TGAHeader header; // struct for the header info
-    char* pixels, *buffer;
+    FILE *file = NULL; // the file handle
+    TGAHeader header;  // struct for the header info
+    char *pixels, *buffer;
 
-	// column counter, row counter, i & j loop counters, and bytes per pixel
+    // column counter, row counter, i & j loop counters, and bytes per pixel
     int c, r, i, bytespp = 4;
     char n, packet_header;
     char pixel[4];
@@ -45,25 +46,25 @@ TGA::TGA(const char* imagePath)
 
     bytespp = header.bpp / 8; // bytes per pixel
 
-	pixels = (char*)malloc(bytespp * header.width * header.height);
+    pixels = (char *)malloc(bytespp * header.width * header.height);
 
     // header type 2 is uncompressed RGB data without a color map / pallette
     if (header.type == 2)
-	{
+    {
         // seek to the start of the data
         fseek(file, header.map_start + header.map_length * bytespp + 18, SEEK_SET);
 
         // read the pixel data into a buffer
-        buffer = (char*)malloc(bytespp * header.width * header.height);
+        buffer = (char *)malloc(bytespp * header.width * header.height);
         fread(buffer, bytespp, header.width * header.height, file);
 
         // plot the pixel data into pixels buffer
-        for (c = 0; c < header.width; c ++) // count up columns
-		{
-            for (r = 0; r < header.height; r ++) // rows
-			{
+        for (c = 0; c < header.width; c++) // count up columns
+        {
+            for (r = 0; r < header.height; r++) // rows
+            {
                 if (bytespp == 4)
-				{
+                {
                     pixels[(c + (header.height - r - 1) * header.width) * bytespp + 0] =
                         buffer[(c + r * header.width) * bytespp + 2];
                     pixels[(c + (header.height - r - 1) * header.width) * bytespp + 1] =
@@ -73,8 +74,8 @@ TGA::TGA(const char* imagePath)
                     pixels[(c + (header.height - r - 1) * header.width) * bytespp + 3] =
                         buffer[(c + r * header.width) * bytespp + 3];
                 }
-				else if (bytespp == 3)
-				{
+                else if (bytespp == 3)
+                {
                     pixels[(c + (header.height - r - 1) * header.width) * bytespp + 0] =
                         buffer[(c + r * header.width) * bytespp + 2];
                     pixels[(c + (header.height - r - 1) * header.width) * bytespp + 1] =
@@ -86,35 +87,35 @@ TGA::TGA(const char* imagePath)
         }
 
         free(buffer);
-
     }
-	else if (header.type == 10) // run length encoded, non color mapped rgb
-	{
+    else if (header.type == 10) // run length encoded, non color mapped rgb
+    {
         // find the start of the data
         fseek(file, header.map_start + header.map_length * bytespp + 18, SEEK_SET);
 
-        c = 0; r = header.height - 1; // start at the top left
+        c = 0;
+        r = header.height - 1; // start at the top left
         // work through the bitmap
         while (r >= 0)
-		{
+        {
             // read in the packet header
             fread(&packet_header, sizeof(packet_header), 1, file);
 
             // find the number of reps
             n = packet_header & 0x7F;
 
-            *((int*)pixel) = 0;
+            *((int *)pixel) = 0;
             if (n != packet_header) // if bit = 1, the next pixel repeated N times
                 fread(&pixel, bytespp, 1, file);
 
             // loop n times
             for (i = 0; i < n + 1; i++)
-			{
+            {
                 if (n == packet_header) // if bit = 0, N individual pixels
                     fread(&pixel, bytespp, 1, file);
 
                 if (bytespp == 4)
-				{
+                {
                     pixels[(c + r * header.width) * bytespp + 0] =
                         pixel[2];
                     pixels[(c + r * header.width) * bytespp + 1] =
@@ -124,8 +125,8 @@ TGA::TGA(const char* imagePath)
                     pixels[(c + r * header.width) * bytespp + 3] =
                         pixel[3];
                 }
-				else if (bytespp == 3)
-				{
+                else if (bytespp == 3)
+                {
                     pixels[(c + r * header.width) * bytespp + 0] =
                         pixel[2];
                     pixels[(c + r * header.width) * bytespp + 1] =
@@ -136,7 +137,7 @@ TGA::TGA(const char* imagePath)
                 // move to the next pixel
                 c += 1;
                 if (c >= header.width)
-				{
+                {
                     c = 0;
                     r -= 1;
                 }
@@ -146,28 +147,28 @@ TGA::TGA(const char* imagePath)
     // close the file
     fclose(file);
 
-	// make a gl texture
-	glGenTextures(1, &textureHandle);
+    // make a gl texture
+    glGenTextures(1, &textureHandle);
 
-	glBindTexture(GL_TEXTURE_2D, textureHandle);
+    glBindTexture(GL_TEXTURE_2D, textureHandle);
 
-	glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 
-	glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
-					 GL_LINEAR_MIPMAP_NEAREST );
-	glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                    GL_LINEAR_MIPMAP_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
-	glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-	gluBuild2DMipmaps( GL_TEXTURE_2D, 3, header.width, header.height,
-                   GL_RGB, GL_UNSIGNED_BYTE, pixels);
+    gluBuild2DMipmaps(GL_TEXTURE_2D, 3, header.width, header.height,
+                      GL_RGB, GL_UNSIGNED_BYTE, pixels);
 
-	// delete the pixel data
-	free(pixels);
+    // delete the pixel data
+    free(pixels);
 }
 
 GLuint TGA::getTextureHandle(void)
 {
-	return textureHandle;
+    return textureHandle;
 }
